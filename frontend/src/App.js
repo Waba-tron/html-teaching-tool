ce5943170318476bfdd0bfa9a54bf11a689d4928
Wahab Rehman <wahab_rehman1256@outlook.com>
2020-12-18 22:02:28 +0000

    finished-signin-frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/Nav/Navbar.component';
 import HomePage from './Screens/HomePage';
 import SigninScreen from './Screens/SignInPage/Signin-screen';
+import SignUpScreen from './Screens/SignUpPage/SignUp-screen.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/Signin" component={SigninScreen}/>
+        <Route path="/Signup" component={SignUpScreen}/>
       </Switch>
     </div>
 
--- frontend/src/Screens/SignInPage/Signin-screen.jsx
+++ frontend/src/Screens/SignInPage/Signin-screen.jsx
@@ -2,17 +2,33 @@ import React, {useState} from 'react';
 import '../../design-tokens/_scss-varibles.scss';
 import FormInput from '../../components/Form-Inputs/Form-Input.component';
 import Button from '../../components/Buttons/Button.component';
+import {Link} from 'react-router-dom';
 import './Signin.styles.scss';
 
 const SigninScreen = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    let coverImageStyle ={
+        backgroundImage: `url(https://blog.udacity.com/wp-content/uploads/2020/06/HTML_Blog-scaled.jpeg) , linear-gradient(90deg, rgb(28 27 27 / 80%) 0%, rgb(26 23 23 / 70%) 100%)`,
+    }
+
     return (
         <div className="login-container">
 
-            <div className="login-form-img">
-                <img src="https://blog.udacity.com/wp-content/uploads/2020/06/HTML_Blog-scaled.jpeg" alt="Girl in a jacket" />
+            <div className="login-form-img" style={coverImageStyle}>
+                <div className="signin-logo-container">
+                <div>
+                <i class="fab fa-codepen"></i> 
+                </div>
+
+          
+
+             
+         
+                </div>
+                 
             </div>
 
 
@@ -24,7 +40,7 @@ const SigninScreen = () => {
                     name='email'
                     value={email}
                     onChange={e => setEmail(e.target.value)} 
-                    label="email"
+                    label="Email"
                     required
                 />
                 <FormInput name="password"
@@ -33,15 +49,14 @@ const SigninScreen = () => {
                     handleChange={e => setPassword(e.target.value)}
                     label="Password"
                 />
-                
-           
-
-
 
                 <div className="btn-container">
                     <Button text={'Sign In'}/>
                 </div>
-              
+
+                <div className="btn-container">
+                    <Link to="Signup">Registar</Link>
+                </div>
                 </form>
 
             </div>
