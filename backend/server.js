a13ecf3f86e862395837c63e5d2494ed0cde074f
wahabDev1999 <wahab_rehman1256@outlook.com>
2021-05-04 18:42:03 +0100

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -17,10 +18,19 @@ app.use("/api/quiz", quizRoutes);
 app.use("/api/questionnaire", questionnaireRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Backend is running");
-});
+const __dirname = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 
